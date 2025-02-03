7def28dda9e346884ee18d0aff8d49d720a92315
Andrii Kubov <AKUV@xpandsoftware.com>
2025-02-03 19:10:06 +0200

    update glob

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 /**
- * Takes the incomming stream of files and outputs a new file containing a list of @imports rule in sass.
+ * Takes the incoming stream of files and outputs a new file containing a list of @imports rule in sass.
  */
 var slash = require('slash');
 var path = require('path');
@@ -17,7 +17,7 @@ module.exports = function() {
         var reg = /(@forward|@use)\s+["']([^"']+\*(\.scss)?)["'](| as \*)?/;
         var result;
 
-        while((result = reg.exec(contents)) !== null) {
+        while ((result = reg.exec(contents)) !== null) {
             var index = result.index;
             var importRule = result[0];
             var globPattern = result[2];
@@ -28,41 +28,43 @@ module.exports = function() {
 
             if (importRule.indexOf(' as ') !== -1) {
                 isAs = true;
-                asRule = importRule.split(' as ').pop();
-                asRule = asRule.replace(';','');
+                asRule = importRule.split(' as ').pop().replace(';', '');
             }
 
             if (importRule.indexOf('@forward') !== -1) {
                 isForward = true;
             }
 
-            var files = glob.sync(path.join(file.base, globPattern), {
-                cwd: file.base
-            });
-
-            files.forEach(function(filename){
-                // check if it is a sass file
-                if (path.extname(filename).toLowerCase() == '.scss') {
-                    // we remove the parent file base path from the path we will output
-                    filename = path.normalize(filename);
-                    var base = path.join(path.normalize(file.base), '/');
-
-                    filename = filename.replace(base, '');
-                    const type = isForward ? '@forward' :'@use';
-                    let importPath = `${type} "${slash(filename)}"`;
-                    if (isAs) {
-                        importPath = `${importPath} as ${asRule}`;
-                    }
-                    imports.push(`${importPath};`);
+            // Asynchronous glob search
+            glob(path.join(file.base, globPattern), { cwd: file.base }, function(err, files) {
+                if (err) {
+                    return cb(err); // Propagate errors if glob fails
                 }
+
+                files.forEach(function(filename) {
+                    // check if it is a sass file
+                    if (path.extname(filename).toLowerCase() === '.scss') {
+                        // we remove the parent file base path from the path we will output
+                        filename = path.normalize(filename);
+                        var base = path.join(path.normalize(file.base), '/');
+                        filename = filename.replace(base, '');
+                        
+                        const type = isForward ? '@forward' : '@use';
+                        let importPath = `${type} "${slash(filename)}"`;
+                        if (isAs) {
+                            importPath = `${importPath} as ${asRule}`;
+                        }
+                        imports.push(`${importPath};`);
+                    }
+                });
+
+                var replaceString = imports.join('\n');
+                contents = contents.replace(importRule, replaceString);
+                file.contents = Buffer.from(contents);
+
+                cb(null, file);
             });
-
-            var replaceString = imports.join('\n');
-            contents = contents.replace(importRule, replaceString);
-            file.contents = new Buffer(contents);
         }
-
-        cb(null, file);
     };
     return through.obj(transform);
 };
